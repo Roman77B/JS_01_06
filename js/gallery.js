3f694724ad6ad247f48fdec408c3a5343572036c
Roman77B <rbodrov@gmail.com>
2020-02-08 21:19:45 +0300

    1st commit

---
+++ js/gallery.js
@@ -0,0 +1,54 @@
+﻿// БД
+const pics = [
+  "https://cs8.pikabu.ru/post_img/big/2017/12/25/5/1514188160141511997.jpg",
+  "https://robotcomp.ru/wa-data/public/shop/products/40/70/17040/images/24038/24038.150.jpg",
+  "https://s.pn.com.ua/i/md/1022/2519090/2519090_00p.jpg",
+  "https://imprints.ru/script/printers/pic/12858.jpg",
+  "https://avatars.mds.yandex.net/get-mpic/1866068/img_id7997423396899106813.jpeg/6hq"
+]
+
+let main_pic = document.querySelector ("#gal-main")
+let gal_prev = document.querySelector ("#gal-prev")
+let control = document.querySelector ("#control")
+let currentimg = 0
+
+window.onload = () => {
+  main_pic.src = pics [0]
+
+  pics.forEach ((img, index) => {
+    //console.log (index) // отладка
+    gal_prev.insertAdjacentHTML ('beforeend', drawImage (img, index))
+  })
+
+  gal_prev.addEventListener ('click', (event) => {
+    //console.log (event.target.tagName)  //отладка
+    if (event.target.tagName === 'IMG') {
+      main_pic.src = event.target.src
+      currentimg = +(event.target.id.replace('pic-', '')) //запишем сюда index картинки
+    }
+  })
+
+  control.addEventListener ('click', (event) => {
+    if (event.target.name === "control") {
+      let step = +event.target.dataset.step
+      //console.log (`шаг ${step} картинка ${currentimg}`) //отладка
+      if (currentimg + step < 0) {
+        main_pic.src = pics [pics.length - 1] 
+        currentimg = pics.length - 1
+      } else if (currentimg + step === pics.length) {
+        main_pic.src = pics [0] 
+        currentimg = 0
+      } else { 
+        main_pic.src = pics [currentimg + step] 
+        currentimg += step
+      }     
+      //console.log (`картинка ${currentimg}`) // отладка
+    }
+  })
+}
+
+function drawImage (src, i) {
+  return `
+    <img id="pic-${i}" src="${src}" width="200" height="150">
+  `
+}
---
+++ js/main.js
@@ -0,0 +1,92 @@
+﻿// БД
+let PRODUCT_NAMES = ["Ноутбук","Монитор","Процессор","Принтер","Клавиатура","Мышь","Колонки","Gamepad"]
+let PRODUCT_PRICES = ["1000","200","1000","100","20","10","30","20"]
+let PRODUCT_IDS = ["0","1","2","3","4","5","6","7"]
+
+// Массив объектов товаров
+let products = []
+
+// Функция возвращает объект - товар по индексу массивов PRODUCT_NAMES, PRICES, IDS
+function createProduct (index) {
+  return {
+    product_name: PRODUCT_NAMES [index],
+    product_price: PRODUCT_PRICES [index],
+    product_id: PRODUCT_IDS [index],
+    // в каждом продукте будет храниться его кусок верстки
+    // + dataset параметры
+    createTemplate () {
+      return `
+        <div class="shop-item-wrp" data-id="${this.product_id}">
+          <img class="shop-item-pic" src="https://placehold.it/200x150" alt="${this.product_name}" width="200" height="150">
+          <h3 class="shop-item-h3">${this.product_name}</h3>
+          <span class="shop-item-price">${this.product_price}</span>
+          <button class="shop-item-button"
+          data-id="${this.product_id}"
+          data-name="${this.product_name}"
+          data-price="${this.product_price}">
+            Купить
+          </button>
+        </div>
+      `
+    }
+  }
+}
+
+// корзина глобальная
+let cart = {
+  items: [],
+  container: '.cart'
+} 
+
+// Функция возвращает объект товара для вставки в массив items в корзине
+function addProductToBasket (id) {
+  console.log (id)
+  let product = {
+    product_id: PRODUCT_IDS [id],
+    product_name: PRODUCT_NAMES [id],
+    product_price: PRODUCT_PRICES [id]
+  }
+
+  cart.items.push (product)
+}
+
+// Cоздание объекта каталога товаров
+let catalog = {
+  items: [],
+  container: '.shop',
+  cart: null, // ссылка на корзину
+
+  init () {
+    this._fetchItems ()
+    this._render ()
+    this.cart = cart
+
+    document.querySelector ('.shop').addEventListener ('click', (event) => {
+      if (event.target.className === 'shop-item-button') {
+        console.log (event)
+        addProductToBasket (event.target.dataset.id) 
+      }
+    })
+
+    
+    //console.log (this.cart)
+  },
+
+  _fetchItems () {
+    let length = PRODUCT_IDS.length
+    for (let i = 0; i <= length - 1; i++) {
+      this.items.push (createProduct (i))
+    }
+  },
+
+  _render () {
+    let container = document.querySelector (this.container)
+    let domString = ""
+    this.items.forEach (item => {
+       domString += item.createTemplate ()
+    })
+    container.innerHTML = domString
+  }
+}
+
+catalog.init () // проинициализровать каталог товаров
